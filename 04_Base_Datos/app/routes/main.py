3aff2f2dbf5d82f4a1accbc1fce72501f56294be
EsneiderCdD <ecd.97@hotmail.com>
2026-01-18 11:06:18 -0500

    04 BD

--- 04_Base_Datos/app/routes/main.py
+++ 04_Base_Datos/app/routes/main.py
@@ -1,16 +1,8 @@
 from flask import Blueprint
-from app.extensions import db
-from sqlalchemy import text  # Para hacer una consulta SQL cruda de prueba
+
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def status():
-    # Pequeña prueba para ver si la DB responde
-    try:
-        db.session.execute(text('SELECT 1'))
-        db_status = "Conectada"
-    except Exception as e:
-        db_status = f"Error: {str(e)}"
-        
-    return f"Backend6 Running (PostgreSQL Status: {db_status})"
+    return "04_Base_Datos Running"
