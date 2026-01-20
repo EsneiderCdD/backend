9c77a5fd792e39d037ccec07560344f93ca5aaf9
EsneiderCdD <ecd.97@hotmail.com>
2026-01-19 22:35:57 -0500

    04 Base de Datos

--- 04_Base_Datos/app/__init__.py
+++ 04_Base_Datos/app/__init__.py
@@ -2,20 +2,16 @@ from flask import Flask
 from app.config.config import Config
 from app.extensions import db, migrate
 from app.routes import main
-# Importamos modelos para que Flask-Migrate los detecte
 from app.models.usuario import Usuario
 
 def create_app():
     app = Flask(__name__)
     
-    # 1. Cargar Configuración (incluyendo DATABASE_URL)
     app.config.from_object(Config)
     
-    # 2. Inicializar Extensiones con la app
     db.init_app(app)
     migrate.init_app(app, db)
     
-    # 3. Registrar Blueprints
     app.register_blueprint(main.bp)
     
     return app
--- 04_Base_Datos/app/models/usuario.py
+++ 04_Base_Datos/app/models/usuario.py
@@ -1,11 +1,7 @@
 from app.extensions import db
 
 class Usuario(db.Model):
-    # Definimos la tabla 'usuarios'
     __tablename__ = 'usuarios'
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    
-    def __repr__(self):
-        return f'<Usuario {self.username}>'
